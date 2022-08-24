524560c4ac548f974072c66d7e0c03e0e9d84eca
Tcaf351 <74075640+Tcaf351@users.noreply.github.com>
2022-08-24 19:48:55 +1000

    final beginner lesson

---
+++ day14.py
@@ -0,0 +1,29 @@
+# Higher or Lower Game
+
+# Generate a random account from the game data
+
+
+# Format the account data into printable format
+
+
+# Ask user for a guess
+
+
+# Check if user is correct
+## Get follower count of each account
+## User if statement to check if user if correct
+
+
+# Give user feedback on their guess.
+
+
+# Score keeping
+
+
+# Make the game repeatable
+
+
+# Make account at position b become the next account at position a
+
+
+# Clear the screen 
