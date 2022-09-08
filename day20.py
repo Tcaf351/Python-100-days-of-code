abf8b87258b4f0a891fe83dff3dc7a2a85711633
Tcaf351 <74075640+Tcaf351@users.noreply.github.com>
2022-09-08 16:37:21 +1000

    created a snake class, looked at keystrokes

--- day20.py
+++ day20.py
@@ -1,6 +1,6 @@
 # Building the snake game part 1 - Animation & Coordinates
-from tracemalloc import stop
 from turtle import Turtle, Screen
+from snake import Snake
 import time
 
 screen = Screen()
@@ -8,35 +8,21 @@ screen.setup(width=600, height=600) # setting the size of the map
 screen.bgcolor("black") # setting the background color
 screen.title("My Snake Game")
 screen.tracer(0)
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
-segments = []
+snake = Snake()
 
-
-for position in starting_positions:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
-    
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
 
-    for seg_num in range(len(segments) - 1, 0 , -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
-
-
-
-
-
-
+    snake.move()
 
 
 
